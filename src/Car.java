fdf6523e911bb8197a08de93f0eebbbb2f28c91b
michael-sey-23 <voluble153@gmail.com>
2025-12-07 15:33:56 +0000

    Corrected spelling

--- src/Car.java
+++ src/Car.java
@@ -1,5 +1,5 @@
 public class Car extends Vehicle{
-    public Car(FuelType fuelType, double annualMileage, double rateOFFuelConsumption, double engineSize){
-        super("Car",fuelType,annualMileage, rateOFFuelConsumption, engineSize);
+    public Car(FuelType fuelType, double annualMileage, double rateOfFuelConsumption, double engineSize){
+        super("Car",fuelType,annualMileage, rateOfFuelConsumption, engineSize);
     }
 }
