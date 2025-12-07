ae447c80ee8b47d16d672d956a0c7c15421b0fd4
michael-sey-23 <voluble153@gmail.com>
2025-12-07 14:53:03 +0000

    made changes to diet emissions calculation

--- src/Diet.java
+++ src/Diet.java
@@ -5,13 +5,34 @@ public class Diet extends EmissionSource {
     public Diet(DietType dietType, double annualEnergyConsumptionKWh){
         super();
         this.name = "Diet";
-        this.category = "";
+        this.category = "Food";
         this.annualEnergyConsumptionKWh = annualEnergyConsumptionKWh;
         this.dietType = dietType;
     }
 
+    /**
+     * Calculates annual carbon emissions from diet.
+
+     * Average annual emissions by diet type:
+     * VEGAN: ~1,500 kg CO₂ per year
+     * NON-VEGAN: ~2,500 kg CO₂ per year
+
+     * These values represent typical carbon footprints from food production,
+     * including agriculture, livestock, processing, and transportation.
+
+     * The annualEnergyConsumptionKWh can represent cooking/refrigeration energy,
+     * with a conversion factor of 0.233 kg CO₂ per kWh (electricity).
+     */
     @Override
     public double calculateEmission() {
+        double dietEmission = switch(dietType){
+            case VEGAN -> 1500.0;
+            case NONVEGAN -> 2500.0;
+        };
 
+        // Add emissions from cooking/refrigeration energy
+        double energyEmission = annualEnergyConsumptionKWh * 0.233;
+
+        return dietEmission + energyEmission;
     }
 }
