ab45327f0d9851738a08c36e4c15360d6b068814
michael-sey-23 <voluble153@gmail.com>
2025-12-07 15:35:11 +0000

    Removed annualEnergyConsumptionKWh, not being used.

--- src/Diet.java
+++ src/Diet.java
@@ -1,12 +1,10 @@
 public class Diet extends EmissionSource {
     protected DietType dietType;
-    protected double annualEnergyConsumptionKWh;
 
-    public Diet(DietType dietType, double annualEnergyConsumptionKWh){
+    public Diet(DietType dietType){
         super();
         this.name = "Diet";
         this.category = "Food";
-        this.annualEnergyConsumptionKWh = annualEnergyConsumptionKWh;
         this.dietType = dietType;
     }
 
